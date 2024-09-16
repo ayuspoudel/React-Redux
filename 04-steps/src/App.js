0d9de8cfafcab6150bec1d46d5ceb1db9145662b
Ayush Poudel <ayushpoudel@usf.edu>
2024-09-16 15:54:31 -0400

    Add handler functions and modify the code

--- 04-steps/src/App.js
+++ 04-steps/src/App.js
@@ -1,4 +1,38 @@
-import logo from "./logo.svg";
-import "./App.css";
+const message = ["Learn React", "Apply for jobs", "Invest your new income"];
 
-export default App;
+export default function App() {
+  const step = 3;
+  function handlePrevious() {
+    alert("Previous");
+  }
+  function handleNext() {
+    alert("Next");
+  }
+  return (
+    <div className="steps">
+      <div className="numbers">
+        <div className={`${step >= 1 ? "active" : ""}`}>1</div>
+        <div className={`${step >= 1 ? "active" : ""}`}>2</div>
+        <div className={`${step >= 1 ? "active" : ""}`}>3</div>
+      </div>
+      <p className="message">
+        Step {step}: {message[step - 1]}
+      </p>
+
+      <div className="buttons">
+        <button
+          style={{ backgroundColor: "#7950f2", color: "#ffff" }}
+          onClick={handlePrevious}
+        >
+          Previous
+        </button>
+        <button
+          style={{ backgroundColor: "#7950f2", color: "#ffff" }}
+          onClick={handleNext}
+        >
+          Next
+        </button>
+      </div>
+    </div>
+  );
+}
